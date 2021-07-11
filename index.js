f1f1793327ae1f159f9160472c73da18de20ae96
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-11 13:13:11 -0400

    view all departments

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection.js');
-const { addRole, removeRole, getAllEmployees, getAllRoles } = require('./utils/queries');
+const { addRole, addEmployee, addDepartment, removeRole, getAllEmployees, getAllRoles, updateRole, getAllDepartments } = require('./utils/queries');
 
 const showMenu = () => {
     return inquirer.prompt([
@@ -8,7 +8,7 @@ const showMenu = () => {
             type: 'list',
             name: 'menu',
             message: 'What would you like to do?',
-            choices: ['View All Employees', 'View All Employees By Department', 'View All Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles', 'Add Role', 'Remove Role', 'Exit'],
+            choices: ['View All Employees', 'View All Departments', 'View All Employees By Manager', 'Add a Department', 'Add Employee', 'Remove Employee', 'Update Employee Role', 'Update Employee Manager', 'View All Roles', 'Add Role', 'Remove Role', 'Exit'],
             validate: menuInput => {
                 if (menuInput) {
                     return true;
@@ -35,6 +35,15 @@ const selectionHandler = (data) => {
     }
     else if (data.menu == 'Remove Role') {
         removeRolePrompt();
+    }
+    else if (data.menu == 'Add Employee') {
+        addEmployeePrompt();
+    }
+    else if (data.menu == 'Update Employee Role') {
+        updateRolePrompt();
+    }
+    else if (data.menu == 'View All Departments') {
+        getAllDepartments();
     }
     else if (data.menu == 'Exit') {
         db.end();
@@ -92,6 +101,110 @@ const removeRolePrompt = () => {
     ]).then(data => {
         removeRole(data.id);
     })
+}
+
+const addEmployeePrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first',
+            message: `What is the first name of the employee?`,
+            validate: firstName => {
+                if (firstName) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'last',
+            message: `What is the last name of the employee?`,
+            validate: lastName => {
+                if (lastName) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: `What is the role of the employee?`,
+            validate: roleTitle => {
+                if (roleTitle) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: `Who is the employee's Manager?`,
+            validate: managerName => {
+                if (managerName) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    ]).then(data => {
+        addEmployee(data.first, data.last, data.role, data.manager);
+    })
+}
+
+let choiceArray = [];
+
+const employeeList = () => {
+    db.promise().query(`SELECT first_name, last_name FROM employee`)
+        .then(([rows]) => {
+            for (i = 0; i < rows.length; i++) {
+                let employee = console.log(rows[i].first_name + ' ' + rows[i].last_name)
+                choiceArray.push(employee);
+            }
+        })
+}
+
+const updateRolePrompt = () => {
+    employeeList();
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'name',
+            message: 'Select the employee you want to update',
+            choices: choiceArray
+        }
+    ])
+}
+
+const addDepartmentPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'deptName',
+            message: 'What is the department name?',
+            validate: deptName => {
+                if (deptName) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+    ])
+        .then(data => {
+            addDepartment(data.deptName)
+        })
 }
 
 showMenu();
--- utils/queries.js
+++ utils/queries.js
@@ -34,7 +34,6 @@ const addRole = (title, salary) => {
 }
 
 const removeRole = (id) => {
-    console.log(id);
     const sql = `DELETE FROM role WHERE id = ?`;
     const params = [id];
     db.promise().query(sql, params)
@@ -42,4 +41,37 @@ const removeRole = (id) => {
         .catch(console.log);
 }
 
-module.exports = { addRole, removeRole, getAllEmployees, getAllRoles };
+const addEmployee = (first, last, role, manager) => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    const params = [first, last, role, manager];
+    db.promise().query(sql, params)
+        .then(getAllEmployees())
+        .catch(console.log);
+}
+const updateRole = (emp_id, role_id) => {
+    const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+    const params = [emp_id, role_id];
+    db.promise().query(sql, params)
+        .then(getAllEmployees())
+        .catch(console.log);
+}
+
+const getAllDepartments = () => {
+    db.promise().query(`SELECT * FROM department`)
+        .then(([rows]) => {
+            console.table(rows);
+        })
+        .then(db.end())
+        .catch(console.log)
+}
+
+const addDepartment = (department) => {
+    const sql = `INSERT INTO department (dept_name) VALUES (?)`;
+    const params = [department];
+    db.promise().query(sql, params)
+        .then(getAllDepartments())
+        .catch(console.log);
+
+}
+
+module.exports = { addRole, removeRole, addEmployee, getAllEmployees, getAllRoles, updateRole, getAllDepartments, addDepartment }
