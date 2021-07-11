4d0facb04d8f2dc71f57997f0e0703ea789d3cae
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-10 21:24:08 -0400

    employee table

--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE role (
   salary DECIMAL(9, 2) NOT NULL,
   CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id)
+);
